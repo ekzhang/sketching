c9752d002292a290463e9001ae8e96385f750610
Victor Rong <vrong@mit.edu>
2021-05-09 20:43:06 -0400

    Implemented curvature computation (Rusinkiewicz 2004) in python

--- scripts/curvature.py
+++ scripts/curvature.py
@@ -1,12 +1,14 @@
 import open3d as o3d
 import numpy as np
 from scipy.linalg import eig
+from scipy.spatial.transform import Rotation
 import mcubes #pip install --upgrade PyMCubes
 import json
 import argparse
 
 # -- Begin primitive SDFs from
 # https://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+
 
 def length(p):
     return np.linalg.norm(p, axis=-1)
@@ -59,7 +61,7 @@ def mesh_from_sdf(example, n=40):
     mesh.triangles = o3d.utility.Vector3iVector(triangles[:,::-1])
     return mesh
 
-def compute_curvature_directions(mesh):
+def compute_curvature_directions_taubin(mesh):
     mesh.compute_vertex_normals()
 
     vertices = np.array(mesh.vertices)
@@ -73,6 +75,7 @@ def compute_curvature_directions(mesh):
     matrices = np.zeros((n, 3, 3), dtype=float)
     curvature_max = np.zeros((n, 3), dtype=float)
     curvature_min = np.zeros((n, 3), dtype=float)
+    confidence = np.zeros((n,), dtype=float)
 
     edges = [[] for i in range(n)]
 
@@ -103,6 +106,7 @@ def compute_curvature_directions(mesh):
             total_area += areas[tri]
             matrices[i,:,:] += areas[tri] * kappa * (t @ t.T)
 
+        #not necessary for finding eigenvectors
         if total_area < 1e-8:
             total_area = 1
         matrices[i,:,:] /= total_area
@@ -126,6 +130,127 @@ def compute_curvature_directions(mesh):
 
     return curvature_min, curvature_max
 
+def normalize(p):
+    lenp = np.linalg.norm(p)
+    if lenp < 1e-8:
+        return np.array([1,0,0], dtype=float)
+    return p / lenp
+
+def compute_voronoi_area(a, b, c):
+    area = 0.5 * np.linalg.norm(np.cross(a-b, a-c))
+    if area < 1e-8:
+        return [area/3, area/3, area/3]
+    if np.dot(a-b, a-c) < 0 or np.dot(b-c, b-a) < 0 or np.dot(c-a, c-b) < 0:
+        return [area/3, area/3, area/3]
+    ta = (np.linalg.norm(b-c)/2)**2 * np.dot(a-b, a-c)/np.linalg.norm(np.cross(a-b, a-c))
+    tb = (np.linalg.norm(c-a)/2)**2 * np.dot(b-c, b-a)/np.linalg.norm(np.cross(b-c, b-a))
+    tc = (np.linalg.norm(a-b)/2)**2 * np.dot(c-a, c-b)/np.linalg.norm(np.cross(c-a, c-b))
+    return [(tb+tc)/2, (tc+ta)/2, (ta+tb)/2]
+
+def compute_curvature_directions_rusinkiewicz(mesh):
+    mesh.compute_vertex_normals()
+
+    vertices = np.array(mesh.vertices)
+    triangles = np.array(mesh.triangles)
+
+    n = vertices.shape[0]
+    m = triangles.shape[0]
+
+    normals = np.array(mesh.vertex_normals)
+    vertex_areas = np.zeros((n), dtype=float)
+    matrices = np.zeros((n, 2, 2), dtype=float)
+    coordinates = np.zeros((n, 3, 3), dtype=float)
+    curvature_max = np.zeros((n, 3), dtype=float)
+    curvature_min = np.zeros((n, 3), dtype=float)
+    confidence = np.zeros((n,), dtype=float)
+
+    for i in range(n):
+        test = np.array([1, 0, 0], dtype=float)
+        if np.linalg.norm(np.cross(test, normals[i])) < 1e-2:
+            test = np.array([0, 1, 0], dtype=float)
+        coordinates[i,0,:] = test - np.dot(test, normals[i]) * normals[i]
+        coordinates[i,1,:] = normals[i]
+        coordinates[i,2,:] = normalize(np.cross(coordinates[i,0,:], coordinates[i,1,:]))
+
+    for i in range(m):
+        triangle = vertices[triangles[i,0]], vertices[triangles[i,1]], vertices[triangles[i,2]]
+        areas = compute_voronoi_area(*triangle)
+        for ja in range(3):
+            jb = (ja+1)%3
+            jc = (ja+2)%3
+            idxA = triangles[i,ja]
+            idxB = triangles[i,jb]
+            idxC = triangles[i,jc]
+            tax, tay, taz = coordinates[idxA,0], coordinates[idxA,1], coordinates[idxA,2]
+            area = areas[ja]
+            if area < 1e-8:
+                continue
+            ax = normalize(vertices[idxB] - vertices[idxA])
+            ay = normalize(np.cross(vertices[idxB] - vertices[idxA], vertices[idxC] - vertices[idxA]))
+            az = normalize(np.cross(ax, ay))
+            proj = np.array([ax, ay, az])
+
+            edgeA = (proj @ (vertices[idxC] - vertices[idxB]))[[0,2]]
+            edgeB = (proj @ (vertices[idxC] - vertices[idxA]))[[0,2]]
+            edgeC = (proj @ (vertices[idxB] - vertices[idxA]))[[0,2]]
+
+            #inv(proj.T) = proj
+            dnA = (proj @ (normals[idxC] - normals[idxB]))[[0,2]]
+            dnB = (proj @ (normals[idxC] - normals[idxA]))[[0,2]]
+            dnC = (proj @ (normals[idxB] - normals[idxA]))[[0,2]]
+
+            A = np.array(
+                [
+                    [edgeA[0], edgeA[1], 0],
+                    [0, edgeA[0], edgeA[1]],
+                    [edgeB[0], edgeB[1], 0],
+                    [0, edgeB[0], edgeB[1]],
+                    [edgeC[0], edgeC[1], 0],
+                    [0, edgeC[0], edgeC[1]],
+                ],
+                dtype=float
+            )
+            b = np.array(
+                [
+                    dnA[0],
+                    dnA[1],
+                    dnB[0],
+                    dnB[1],
+                    dnC[0],
+                    dnC[1],
+                ],
+                dtype=float
+            )
+
+            x = np.linalg.inv(A.T @ A) @ A.T @ b
+            LMN_local = np.array([[x[0], x[1]], [x[1], x[2]]], dtype=float)
+
+            rot_axis = normalize(np.cross(ay, tay))
+            rot_theta = np.arccos(np.dot(ay, tay))
+            rot = Rotation.from_rotvec(rot_theta * rot_axis).as_matrix()
+
+            rax = rot @ ax
+            raz = rot @ az
+            L = np.dot(np.array([np.dot(tax, rax), np.dot(tax, raz)]), LMN_local @ np.array([np.dot(tax, rax), np.dot(tax, raz)]))
+            M = np.dot(np.array([np.dot(tax, rax), np.dot(tax, raz)]), LMN_local @ np.array([np.dot(taz, rax), np.dot(taz, raz)]))
+            N = np.dot(np.array([np.dot(taz, rax), np.dot(taz, raz)]), LMN_local @ np.array([np.dot(taz, rax), np.dot(taz, raz)]))
+
+            vertex_areas[idxA] += area
+            matrices[idxA,:,:] += np.array([[L, M], [M, N]]) * area
+    
+    for i in range(n):
+        eigvals, eigvecs = eig(matrices[i])
+        up_proj = coordinates[i,[0,2],:].T
+        if eigvals[0] < eigvals[1]:
+            curvature_min[i,:] = up_proj @ eigvecs[:,0]
+            curvature_max[i,:] = up_proj @ eigvecs[:,1]
+        else:
+            curvature_min[i,:] = up_proj @ eigvecs[:,1]
+            curvature_max[i,:] = up_proj @ eigvecs[:,0]
+        
+
+    return curvature_min, curvature_max
+
 def get_lineset(vertices, vectors, color, l=0.01):
     n = vertices.shape[0]
     vertices = np.tile(vertices, (2, 1))
@@ -144,7 +269,8 @@ def visualize_curvature_directions(mesh, l=0.01, show_normals=False):
     vertices = np.array(mesh.vertices)
     normals = np.array(mesh.vertex_normals)
     n = vertices.shape[0]
-    curvature_min, curvature_max = compute_curvature_directions(mesh)
+    #curvature_min, curvature_max = compute_curvature_directions_rusinkiewicz(mesh)
+    curvature_min, curvature_max = compute_curvature_directions_taubin(mesh)
 
     line_set_max = get_lineset(vertices, curvature_max, [1, 0, 0])
     line_set_min = get_lineset(vertices, curvature_min, [0, 1, 0])
@@ -179,7 +305,7 @@ def write_data(mesh, filename):
     normals = np.array(mesh.vertex_normals).tolist()
     triangles = np.array(mesh.triangles).tolist()
     #curvature_min, curvature_max = [[1,0,0] for i in range(len(vertices))], [[0,1,0] for i in range(len(vertices))]
-    curvature_min, curvature_max = compute_curvature_directions(mesh)
+    curvature_min, curvature_max = compute_curvature_directions_taubin(mesh)
     curvature_min = curvature_min.tolist()
     curvature_max = curvature_max.tolist()
 
@@ -203,6 +329,13 @@ def write_data(mesh, filename):
         json.dump(pretty_floats(data), f)
 
 if __name__ == "__main__":
+    # #mesh = o3d.io.read_triangle_mesh("../models/bunny/reconstruction/bun_zipper.ply")
+    # #mesh = o3d.io.read_triangle_mesh("../models/csg.ply")
+    # mesh = mesh_from_sdf(1, 160)
+    # mesh = center_mesh(mesh)
+    # visualize_curvature_directions(mesh)
+    # #compute_curvature_directions_rusinkiewicz(mesh)
+    # exit()
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--source",
